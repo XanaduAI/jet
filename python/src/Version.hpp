f5debfb9bd2a61c9aaab0a4550de858a6bffd094
Mikhail Andrenkov <Mandrenkov@users.noreply.github.com>
2021-05-14 10:02:19 -0400

    Add CMake build flag for generating Python bindings (#1)
    * Add Python build target and Version.hpp bindings
    
    * Add src/python/Version.hpp to pybind11 source files
    
    * Create changelog entry for 0.2.0 release
    
    * Add src/ directory to Format workflow
    
    * Add src/ directory to list of Format workflow paths
    
    * Add proper build system support for Python bindings
    
    * Restore requirements_test.txt touch file
    
    * Create GitHub Actions workflow for Python bindings
    
    * Move 'make setup' to independent step
    
    * Replace src/ directory with python/ directory in formatters
    
    * Acknowledge Jack's migrated contribution to the Tensor.hpp bindings branch
    
    * Replace python/ with python/src/ in formatters
    
    * Declare pybind11 target only if BUILD_PYTHON is ON
    
    * Use FetchContent_MakeAvailable() to fetch Catch2
    
    * Set indentation to 2 spaces
    
    * Move Python bindings commands to CMake script in python/ directory
    
    * Add Python formatter to GitHub Actions workflow
    
    * Add context to status check names

---
+++ python/src/Version.hpp
@@ -0,0 +1,22 @@
+#include <pybind11/pybind11.h>
+
+#include <Jet.hpp>
+
+namespace py = pybind11;
+
+/**
+ * @brief Adds Python bindings for the include/jet/Version.hpp file.
+ *
+ * @param m Jet pybind11 module.
+ */
+void AddBindingsForVersion(py::module_ &m)
+{
+    m.attr("__version__") = Jet::Version();
+
+    m.def("version", Jet::Version, R"(
+        Returns the current Jet version.
+
+        Returns:
+            String representation of the current Jet version.
+    )");
+}
