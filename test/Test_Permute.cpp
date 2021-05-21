3ea132923b5fca5daa99bb5e0e920f96bd38a3b1
Lee J. O'Riordan <loriordan@gmail.com>
2021-05-21 13:11:14 +0100

    Add initial Permute test

---
+++ test/Test_Permute.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include <catch2/catch.hpp>
+
+#include "jet/Permute.hpp"
+
+using namespace Jet;
+using namespace Jet::Utilities;
+using data_t = std::complex<float>;
+
+TEST_CASE("Permute::GenerateBinaryReorderingMap", "[Permute]")
+{
+    QFPermute qfp;
+
+    std::vector<size_t> data_pos_map_out(16, 0);
+    
+    std::vector<size_t> data_pos_map_expect_03 
+    { 0, 8, 2, 10, 4, 12, 6, 14, 1, 9, 3, 11, 5, 13, 7, 15 };
+    std::vector<size_t> data_pos_map_expect_02 
+    { 0, 1, 8, 9, 4, 5, 12, 13, 2, 3, 10, 11, 6, 7, 14, 15 };
+    std::vector<size_t> data_pos_map_expect_01
+    { 0, 1, 2, 3, 8, 9, 10, 11, 4, 5, 6, 7, 12, 13, 14, 15 };
+    std::vector<size_t> data_pos_map_expect_01_23
+    { 0, 2, 1, 3, 8, 10, 9, 11, 4, 6, 5, 7, 12, 14, 13, 15 };
+
+    qfp.GenerateBinaryReorderingMap({3,1,2,0}, data_pos_map_out);
+    CHECK( data_pos_map_out == data_pos_map_expect_03 );
+    qfp.GenerateBinaryReorderingMap({2,1,0,3}, data_pos_map_out);
+    CHECK( data_pos_map_out == data_pos_map_expect_02 );
+    qfp.GenerateBinaryReorderingMap({1,0,2,3}, data_pos_map_out);
+    CHECK( data_pos_map_out == data_pos_map_expect_01 );
+    qfp.GenerateBinaryReorderingMap({1,0,3,2}, data_pos_map_out);
+    CHECK( data_pos_map_out == data_pos_map_expect_01_23 );
+}
