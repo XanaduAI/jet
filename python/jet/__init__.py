05b7c0869049226dc4995cb761857f8a99aebabe
Mikhail Andrenkov <M.Andrenkov@gmail.com>
2021-05-25 13:52:07 -0400

    Import all bindings directly into the 'jet' package

--- python/jet/__init__.py
+++ python/jet/__init__.py
@@ -1,31 +1,11 @@
-# Copyright 2021 Xanadu Quantum Technologies Inc.
+# The existence of a Python binding is proof that it is intended to be exposed.
+from .bindings import *
 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-from .bindings import PathInfo as PathInfo
-from .bindings import Tensor64 as Tensor
-from .bindings import TensorNetwork64 as TensorNetwork
-from .bindings import TensorNetworkFile64 as TensorNetworkFile
-from .bindings import TensorNetworkSerializer64 as TensorNetworkSerializer
-from .bindings import (
-    add_tensors,
-    conj,
-    contract_tensors,
-    reshape,
-    slice_index,
-    transpose,
-    version,
-)
+# The default Python floating-point type occupies 64 bits.
+Tensor = Tensor64
+TensorNetwork = TensorNetwork64
+TensorNetworkFile = TensorNetworkFile64
+TensorNetworkSerializer = TensorNetworkSerializer64
 
 # Grab the current Jet version from the C++ headers.
 __version__ = version()
