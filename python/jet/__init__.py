c0f3089dbb22938b97d2afa9b59241db909cb5cc
Mikhail Andrenkov <M.Andrenkov@gmail.com>
2021-05-27 18:46:27 -0400

    Replace class parametrizations with dtypes

--- python/jet/__init__.py
+++ python/jet/__init__.py
@@ -6,46 +6,46 @@ from .bindings import *
 
 def Tensor(*args, **kwargs):
     """Contructs a tensor with the specified NumPy data type."""
-    dt = kwargs.pop("dtype", "complex128")
-    if np.dtype(dt) == np.complex64:
+    dtype = kwargs.pop("dtype", "complex128")
+    if np.dtype(dtype) == np.complex64:
         return TensorC64(*args, **kwargs)
-    elif np.dtype(dt) == np.complex128:
+    elif np.dtype(dtype) == np.complex128:
         return TensorC128(*args, **kwargs)
     else:
-        raise TypeError(f"Data type '{dt}' is not supported.")
+        raise TypeError(f"Data type '{dtype}' is not supported.")
 
 
 def TensorNetwork(*args, **kwargs):
     """Contructs a tensor network with the specified NumPy data type."""
-    dt = kwargs.pop("dtype", "complex128")
-    if np.dtype(dt) == np.complex64:
+    dtype = kwargs.pop("dtype", "complex128")
+    if np.dtype(dtype) == np.complex64:
         return TensorNetworkC64(*args, **kwargs)
-    elif np.dtype(dt) == np.complex128:
+    elif np.dtype(dtype) == np.complex128:
         return TensorNetworkC128(*args, **kwargs)
     else:
-        raise TypeError(f"Data type '{dt}' is not supported.")
+        raise TypeError(f"Data type '{dtype}' is not supported.")
 
 
 def TensorNetworkFile(*args, **kwargs):
     """Contructs a tensor network file with the specified NumPy data type."""
-    dt = kwargs.pop("dtype", "complex128")
-    if np.dtype(dt) == np.complex64:
+    dtype = kwargs.pop("dtype", "complex128")
+    if np.dtype(dtype) == np.complex64:
         return TensorNetworkFileC64(*args, **kwargs)
-    elif np.dtype(dt) == np.complex128:
+    elif np.dtype(dtype) == np.complex128:
         return TensorNetworkFileC128(*args, **kwargs)
     else:
-        raise TypeError(f"Data type '{dt}' is not supported.")
+        raise TypeError(f"Data type '{dtype}' is not supported.")
 
 
 def TensorNetworkSerializer(*args, **kwargs):
     """Contructs a tensor network serializer with the specified NumPy data type."""
-    dt = kwargs.pop("dtype", "complex128")
-    if np.dtype(dt) == np.complex64:
+    dtype = kwargs.pop("dtype", "complex128")
+    if np.dtype(dtype) == np.complex64:
         return TensorNetworkSerializerC64(*args, **kwargs)
-    elif np.dtype(dt) == np.complex128:
+    elif np.dtype(dtype) == np.complex128:
         return TensorNetworkSerializerC128(*args, **kwargs)
     else:
-        raise TypeError(f"Data type '{dt}' is not supported.")
+        raise TypeError(f"Data type '{dtype}' is not supported.")
 
 
 # Grab the current Jet version from the C++ headers.
--- python/tests/jet/test_tensor_network_io.py
+++ python/tests/jet/test_tensor_network_io.py
@@ -5,29 +5,23 @@ import pytest
 import jet
 
 
-@pytest.mark.parametrize("TensorNetworkFile", [jet.TensorNetworkFileC64, jet.TensorNetworkFileC128])
-def test_tensor_network_file(TensorNetworkFile):
+@pytest.mark.parametrize("dtype", ["complex64", "complex128"])
+def test_tensor_network_file(dtype):
     """Tests that a tensor network file can be constructed."""
-    tnf = TensorNetworkFile()
+    tnf = jet.TensorNetworkFile(dtype=dtype)
     assert tnf.path is None
     assert len(tnf.tensors.nodes) == 0
 
 
-@pytest.mark.parametrize(
-    "TensorNetworkSerializer",
-    [
-        jet.TensorNetworkSerializerC64,
-        jet.TensorNetworkSerializerC128,
-    ],
-)
+@pytest.mark.parametrize("dtype", ["complex64", "complex128"])
 class TestTensorNetworkSerializer:
     @pytest.fixture
     def tensor_network(self):
         def tensor_network_(dtype: str):
             """Returns a tensor network with three tensors of the given type."""
-            A = jet.Tensor(dtype=dtype, shape=[2, 2], indices=["i", "j"], data=[1, 1j, -1j, 1])
-            B = jet.Tensor(dtype=dtype, shape=[2, 2], indices=["j", "k"], data=[1, 0, 0, 1])
-            C = jet.Tensor(dtype=dtype, shape=[2], indices=["k"], data=[1, 0])
+            A = jet.Tensor(shape=[2, 2], indices=["i", "j"], data=[1, 1j, -1j, 1], dtype=dtype)
+            B = jet.Tensor(shape=[2, 2], indices=["j", "k"], data=[1, 0, 0, 1], dtype=dtype)
+            C = jet.Tensor(shape=[2], indices=["k"], data=[1, 0], dtype=dtype)
 
             tn = jet.TensorNetwork(dtype=dtype)
             tn.add_tensor(A, ["A", "Hermitian"])
@@ -94,54 +88,48 @@ class TestTensorNetworkSerializer:
         }
         """
 
-    def test_serialize_tensor_network(
-        self, tensor_network, TensorNetworkSerializer, serialized_tensor_network
-    ):
+    def test_serialize_tensor_network(self, dtype, tensor_network, serialized_tensor_network):
         """Tests that a tensor network file representing a tensor network can be
         serialized.
         """
-        tn = tensor_network(dtype=TensorNetworkSerializer.dtype)
-        have_json = TensorNetworkSerializer()(tn)
+        tn = tensor_network(dtype=dtype)
+        have_json = jet.TensorNetworkSerializer(dtype=dtype)(tn)
         want_json = re.sub(r"\s+", "", serialized_tensor_network)
         assert have_json == want_json
 
     def test_serialize_tensor_network_and_path(
         self,
+        dtype,
         tensor_network,
-        TensorNetworkSerializer,
         serialized_tensor_network_and_path,
     ):
         """Tests that a tensor network file representing a tensor network and a
         contraction path can be serialized.
         """
-        tn = tensor_network(dtype=TensorNetworkSerializer.dtype)
+        tn = tensor_network(dtype=dtype)
         path = jet.PathInfo(tn=tn, path=[[0, 2], [2, 1]])
-        have_json = TensorNetworkSerializer()(tn, path)
+        have_json = jet.TensorNetworkSerializer(dtype=dtype)(tn, path)
         want_json = re.sub(r"\s+", "", serialized_tensor_network_and_path)
         assert have_json == want_json
 
-    def test_deserialize_tensor_network(
-        self, tensor_network, TensorNetworkSerializer, serialized_tensor_network
-    ):
-        """Tests that a tensor network file representing a tensor network can be
-        deserialized.
-        """
-        tn = tensor_network(dtype=TensorNetworkSerializer.dtype)
-        tnf = TensorNetworkSerializer()(serialized_tensor_network)
+    def test_deserialize_tensor_network(self, dtype, tensor_network, serialized_tensor_network):
+        """Tests that a tensor network file representing a tensor network can be deserialized."""
+        tn = tensor_network(dtype=dtype)
+        tnf = jet.TensorNetworkSerializer(dtype=dtype)(serialized_tensor_network)
         assert tnf.path is None
         assert len(tnf.tensors.nodes) == len(tn.nodes)
 
     def test_deserialize_tensor_network_and_path(
         self,
+        dtype,
         tensor_network,
-        TensorNetworkSerializer,
         serialized_tensor_network_and_path,
     ):
         """Tests that a tensor network file representing a tensor network and a
         contraction path can be deserialized.
         """
-        tn = tensor_network(dtype=TensorNetworkSerializer.dtype)
-        tnf = TensorNetworkSerializer()(serialized_tensor_network_and_path)
+        tn = tensor_network(dtype=dtype)
+        tnf = jet.TensorNetworkSerializer(dtype=dtype)(serialized_tensor_network_and_path)
         assert tnf.path is not None
         assert tnf.path.path == [(0, 2), (2, 1)]
         assert len(tnf.tensors.nodes) == len(tn.nodes)
