65c35bc6a9296e5b41339f98c12e34562c4ae629
Mikhail Andrenkov <M.Andrenkov@gmail.com>
2021-05-27 17:44:25 -0400

    Replace type aliases with factory methods that accept a 'dtype' parameter

--- python/jet/__init__.py
+++ python/jet/__init__.py
@@ -1,12 +1,50 @@
 # The existence of a Python binding is proof of its intention to be exposed.
 from .bindings import *
 
-# By default, Python uses two 64-bit floating-point numbers to represent a
-# complex number. Altogether, this requires 128 bits of storage.
-Tensor = TensorC128
-TensorNetwork = TensorNetworkC128
-TensorNetworkFile = TensorNetworkFileC128
-TensorNetworkSerializer = TensorNetworkSerializerC128
+
+def Tensor(*args, **kwargs):
+    """Contructs a tensor with the specified NumPy data type."""
+    dtype = kwargs.pop("dtype", "c16")
+    if dtype == "c8":
+        return TensorC64(*args, **kwargs)
+    elif dtype == "c16":
+        return TensorC128(*args, **kwargs)
+    else:
+        raise TypeError(f"Data type '{dtype}' is not supported.")
+
+
+def TensorNetwork(*args, **kwargs):
+    """Contructs a tensor network with the specified NumPy data type."""
+    dtype = kwargs.pop("dtype", "c16")
+    if dtype == "c8":
+        return TensorNetworkC64(*args, **kwargs)
+    elif dtype == "c16":
+        return TensorNetworkC128(*args, **kwargs)
+    else:
+        raise TypeError(f"Data type '{dtype}' is not supported.")
+
+
+def TensorNetworkFile(*args, **kwargs):
+    """Contructs a tensor network file with the specified NumPy data type."""
+    dtype = kwargs.pop("dtype", "c16")
+    if dtype == "c8":
+        return TensorNetworkFileC64(*args, **kwargs)
+    elif dtype == "c16":
+        return TensorNetworkFileC128(*args, **kwargs)
+    else:
+        raise TypeError(f"Data type '{dtype}' is not supported.")
+
+
+def TensorNetworkSerializer(*args, **kwargs):
+    """Contructs a tensor network serializer with the specified NumPy data type."""
+    dtype = kwargs.pop("dtype", "c16")
+    if dtype == "c8":
+        return TensorNetworkSerializerC64(*args, **kwargs)
+    elif dtype == "c16":
+        return TensorNetworkSerializerC128(*args, **kwargs)
+    else:
+        raise TypeError(f"Data type '{dtype}' is not supported.")
+
 
 # Grab the current Jet version from the C++ headers.
 __version__ = version()
