dc4e049b5157780667d46f72450fb7fa23bfb3b1
Mikhail Andrenkov <M.Andrenkov@gmail.com>
2021-06-08 15:47:01 -0400

    Expand docstrings of Wire and Circuit classes

--- python/jet/circuit.py
+++ python/jet/circuit.py
@@ -16,6 +16,10 @@ __all__ = [
 
 @dataclass
 class Wire:
+    """Wire represents a collection of tensor indices that are directly or
+    transitively associated with a qudit of a quantum circuit.
+    """
+
     # Position of the wire in the circuit.
     id_: int
     # Number of gates applied to this wire.
@@ -35,8 +39,8 @@ class Circuit:
         of the specified dimension in the vacuum state.
 
         Args:
-            num_wires: number of wires in the circuit.
-            dim: dimension of each wire.
+            num_wires (int): number of wires in the circuit.
+            dim (int): dimension of each wire.
         """
         self._wires = [Wire(i) for i in range(num_wires)]
         self._parts = [Qudit(dim=dim) for _ in range(num_wires)]
@@ -46,16 +50,27 @@ class Circuit:
 
     @property
     def parts(self) -> Tuple[Union[Gate, State]]:
-        """Returns the components of this circuit (i.e., the gates and states)."""
+        """Returns the gates and states that comprise this circuit.  The first
+        ``self.num_wires`` parts are the qudits that begin each wire; other
+        parts appear in the order they were appended to the circuit.
+        """
         return tuple(self._parts)
 
     @property
     def wires(self) -> Tuple[Wire]:
-        """Returns the wires of this circuit."""
+        """Returns the wires of this circuit in increasing order of wire ID."""
         return tuple(self._wires)
 
     def _append_validator(append_fn: Callable) -> Callable:
-        """Decorator which validates the arguments to an append function."""
+        """Decorator which validates the arguments to an append function.
+
+        Args:
+            append_fn (Callable): append function to be validated.
+
+        Returns:
+            Function that validates the passed wire IDs before invoking the
+            decorated append function with the given arguments.
+        """
 
         @wraps(append_fn)
         def validator(self, part: Union[Gate, State], wire_ids: Sequence[int]) -> None:
@@ -80,7 +95,12 @@ class Circuit:
 
     @_append_validator
     def append_gate(self, gate: Gate, wire_ids: Sequence[int]) -> None:
-        """Applies a gate to this circuit along the specified wires."""
+        """Applies a gate along the specified wires.
+
+        Args:
+            gate (Gate): gate to be applied.
+            wire_ids (Sequence[int]): IDs of the wires the gate is applied to.
+        """
         input_indices = self.indices(wire_ids)
 
         for i in wire_ids:
@@ -93,7 +113,12 @@ class Circuit:
 
     @_append_validator
     def append_state(self, state: State, wire_ids: Sequence[int]) -> None:
-        """Terminates the specified wires with a quantum state."""
+        """Terminates the specified wires with a quantum state.
+
+        Args:
+            state (State): state to be used for termination.
+            wire_ids (Sequence[int]): IDs of the wires the state terminates.
+        """
         for i in wire_ids:
             self.wires[i].closed = True
 
@@ -101,11 +126,22 @@ class Circuit:
         self._parts.append(state)
 
     def indices(self, wire_ids: Sequence[int]) -> List[str]:
-        """Returns the index label associated with each of the specified wires."""
+        """Returns the index labels associated with a sequence of wire IDs.
+
+        Args:
+            wire_ids (Sequence[int]): IDs of the wires to get the index labels for.
+
+        Returns:
+            List of index labels.
+        """
         return [self.wires[i].index for i in wire_ids]
 
     def tensor_network(self, dtype: type = np.complex128) -> TensorNetworkType:
-        """Returns the tensor network representation of this circuit."""
+        """Returns the tensor network representation of this circuit.
+
+        Args:
+            dtype (type): data type of the tensor network.
+        """
         tn = TensorNetwork(dtype=dtype)
         for part in self._parts:
             tensor = part.tensor(dtype=dtype)
