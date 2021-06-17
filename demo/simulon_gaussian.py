b37cfbcd16b253a37a104f436e1eaf43ced58bbd
Mikhail Andrenkov <M.Andrenkov@gmail.com>
2021-06-17 10:20:51 -0400

    Replace 'Teleportation' example with GKP program from The Walrus

--- demo/simulon_gaussian.py
+++ demo/simulon_gaussian.py
@@ -1,33 +1,35 @@
 import inspect
 import strawberryfields as sf
 
-# Write a Blackbird program to be executed on the XQC.
+# Write a Blackbird program to prepare an approximate Gottesman-Kitaev-Preskill (GKP) state.
 bbx_script = inspect.cleandoc(
     """
-    name PartialTeleportation
+    name ApproximateGKP
     version 1.0
-    target simulon_gaussian
+    target simulon_gaussian (shots=10)
 
-    Coherent(sqrt(5/4), arctan(1/2)) | 0
-    Sgate(2, 0) | 2
-    Sgate(-2, 0) | 1
-    BSgate(pi/4, 0) | [1, 2]
-    BSgate(pi/4, 0) | [0, 1]
-    MeasureHomodyne(pi/2) | 1
-    MeasureHomodyne(0) | 0
-    # Xgate("1.41421356237*q0") | 2
-    # Zgate("1.41421356237*q1") | 2
+    Sgate(-1.38155106) | 0
+    Sgate(-1.21699567) | 1
+    Sgate(0.779881700) | 2
+
+    BSgate(1.04182349, 1.483536390) | [0, 1]
+    BSgate(0.87702211, 1.696290600) | [1, 2]
+    BSgate(0.90243916, -0.24251599) | [0, 1]
+
+    Sgate(0.1958) | 2
+
+    MeasureFock() | [0, 1, 2]
     """
 )
 
 # Load the Blackbird script into a Strawberry Fields program.
 program = sf.io.loads(bbx_script)
 
-# Create an engine which communicates with "simulon_gaussian" machines.
+# Create an engine which communicates with the "simulon_gaussian" backend.
 engine = sf.RemoteEngine("simulon_gaussian")
 
-# Submit the Strawberry Fields program to the XQC and wait for the results.
-result = engine.run(program, shots=1)
+# Submit the program to the Xanadu Quantum Cloud (XQC) and wait for the results.
+result = engine.run(program)
 
 # Display the returned samples.
 print(f"Samples = {result.samples}")
--- demo/simulon_gaussian_local.py
+++ demo/simulon_gaussian_local.py
@@ -1,22 +1,24 @@
 import inspect
 import strawberryfields as sf
 
-# Write a Blackbird program to be executed with the Gaussian simulator.
+# Write a Blackbird program to prepare an approximate Gottesman-Kitaev-Preskill (GKP) state.
 bbx_script = inspect.cleandoc(
     """
-    name PartialTeleportation
+    name ApproximateGKP
     version 1.0
-    target gaussian
+    target simulon_gaussian (shots=10)
 
-    Coherent(sqrt(5/4), arctan(1/2)) | 0
-    Sgate(2, 0) | 2
-    Sgate(-2, 0) | 1
-    BSgate(pi/4, 0) | [1, 2]
-    BSgate(pi/4, 0) | [0, 1]
-    MeasureHomodyne(pi/2) | 1
-    MeasureHomodyne(0) | 0
-    # Xgate("1.41421356237*q0") | 2
-    # Zgate("1.41421356237*q1") | 2
+    Sgate(-1.38155106) | 0
+    Sgate(-1.21699567) | 1
+    Sgate(0.779881700) | 2
+
+    BSgate(1.04182349, 1.483536390) | [0, 1]
+    BSgate(0.87702211, 1.696290600) | [1, 2]
+    BSgate(0.90243916, -0.24251599) | [0, 1]
+
+    Sgate(0.1958) | 2
+
+    MeasureFock() | [0, 1, 2]
     """
 )
 
@@ -27,7 +29,7 @@ program = sf.io.loads(bbx_script)
 engine = sf.LocalEngine("gaussian")
 
 # Run the program locally and wait for the results.
-result = engine.run(program, shots=1)
+result = engine.run(program)
 
 # Display the returned samples.
 print(f"Samples = {result.samples}")
