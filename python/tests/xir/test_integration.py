1fba3215ae3e9e7288cbb906011174274aca6e5f
Theodor Isacsson <theodor@xanadu.ai>
2021-06-17 19:19:16 -0400

    add tests

--- python/tests/xir/test_integration.py
+++ python/tests/xir/test_integration.py
@@ -46,11 +46,6 @@ gate h(a)[0, 1]:
     // rz(pi / sin(3 * 4 / 2 - 2)) | [a, 2];
 end;
 
-gate H2:
-    H | [0];
-    H | [1];
-end;
-
 operator o(a):
     0.7 * sin(a), X[0] @ Z[1];
     -1.6, X[0];
@@ -69,11 +64,27 @@ h(0.2) | [0, 1, 2];
 sample(observable: o(0.2), shots: 1000) | [0, 1];
 """
 
+jet_script = """
+use xstd;
+
+gate H2:
+    H | [0];
+    H | [1];
+end;
+
+H2 | [0];
+CNOT | [0, 1];
+amplitude(state: [0, 0]) | [0, 1];
+amplitude(state: [0, 1]) | [0, 1];
+amplitude(state: [1, 0]) | [0, 1];
+amplitude(state: [1, 1]) | [0, 1];
+"""
+
 
 class TestParser:
     """Integration tests for parsing, and serializing, XIR scripts"""
 
-    @pytest.mark.parametrize("circuit", [qubit_script, photonics_script, photonics_script_no_decl])
+    @pytest.mark.parametrize("circuit", [qubit_script, photonics_script, photonics_script_no_decl, jet_script])
     def test_parse_and_serialize(self, circuit):
         """Test parsing and serializing an XIR script.
 
