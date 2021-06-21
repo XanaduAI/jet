eedaa65d7ae3dfa0c8dccc92374c65e8a05b021f
Theodor Isacsson <theodor@xanadu.ai>
2021-06-21 17:16:22 -0400

    add parse_script function

--- python/xir/__init__.py
+++ python/xir/__init__.py
@@ -1,1 +1,7 @@
 from .parser import XIRTransformer, xir_parser
+from .program import XIRProgram
+
+def parse_script(circuit: str, eval_pi=False, use_floats=True) -> XIRProgram:
+    """Parse and transform a circuit XIR script and return an XIRProgram"""
+    tree = xir_parser.parse(circuit)
+    return XIRTransformer(eval_pi=eval_pi, use_floats=use_floats).transform(tree)
