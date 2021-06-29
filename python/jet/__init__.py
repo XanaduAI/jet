76c9032c1d43d37344b823f46ba7f0974d768ab6
Mikhail Andrenkov <M.Andrenkov@gmail.com>
2021-06-29 13:21:08 -0400

    Add module docstring to jet package

--- python/jet/__init__.py
+++ python/jet/__init__.py
@@ -1,5 +1,11 @@
+"""The Jet Python API includes bindings for the core Jet C++ library along
+with an interpreter for Xanadu IR (XIR) scripts and a set of classes to model
+quantum gates, states, and circuits.
+"""
+
 # The existence of a Python binding is proof of its intention to be exposed.
 from .bindings import *
+from .bindings import version
 
 # The rest of the modules control their exports using `__all__`.
 from .circuit import *
