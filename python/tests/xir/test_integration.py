680eb603b5861c0db6e000a6d19b45d91a48e865
Theodor <theodor@xanadu.ai>
2021-08-18 17:47:54 -0400

    Better declarations (#61)
    * non-working commit
    
    * fix some more
    
    * add docstrings
    
    * run black
    
    * run isort
    
    * fix test
    
    * resort imports
    
    * run isort again
    
    * Apply suggestions from code review
    
    Co-authored-by: Mikhail Andrenkov <Mandrenkov@users.noreply.github.com>
    
    * add test
    
    * fix params parsing
    
    * format code
    
    * improve tests
    
    * fix failing tests from merge
    
    * fix declarations
    
    * fix params
    
    * update utils tests
    
    * change output decl in test to func
    
    * run black
    
    * run black on tests
    
    * Update python/xir/program.py
    
    Co-authored-by: Mikhail Andrenkov <Mandrenkov@users.noreply.github.com>
    
    * Update python/xir/program.py
    
    Co-authored-by: Mikhail Andrenkov <Mandrenkov@users.noreply.github.com>
    
    * fix error
    
    * run format
    
    * lint
    
    * pin tw to 0.15
    
    * loosen tw requirement
    
    Co-authored-by: Mikhail Andrenkov <Mandrenkov@users.noreply.github.com>

--- python/tests/xir/test_integration.py
+++ python/tests/xir/test_integration.py
@@ -14,13 +14,13 @@ def parse_script(circuit: str) -> XIRProgram:
 
 
 photonics_script = """
-gate Sgate, 2, 1;
-gate BSgate, 2, 2;
-gate Rgate, 1, 1;
-output MeasureHomodyne;
+gate Sgate(a, b)[0];
+gate BSgate(theta, phi)[0, 1];
+gate Rgate(p0)[a];
+output MeasureHomodyne[0];
 
 Sgate(0.7, 0) | [1];
-BSgate(0.1, 0.0) | [0, 1];
+BSgate(0.1, 0.0) | [0, 2];
 Rgate(0.2) | [1];
 
 MeasureHomodyne(phi: 3) | [0];
